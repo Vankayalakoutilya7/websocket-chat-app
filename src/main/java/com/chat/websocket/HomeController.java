10e77ff7691209675b2d20bb81efdc7d7e77f94a
Koutilya <vankayalakoutilya7@gmail.com>
2026-03-16 13:35:59 +0530

    changing home page

--- src/main/java/com/chat/websocket/HomeController.java
+++ src/main/java/com/chat/websocket/HomeController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/")
     public String home() {
         return "WebSocket Chat Backend Running 🚀";
     }
