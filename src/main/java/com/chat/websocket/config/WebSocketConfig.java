b553a254ca5207a773047b164aba85b4bdd5941f
Koutilya <vankayalakoutilya7@gmail.com>
2026-03-16 14:38:47 +0530

    fix cors

--- src/main/java/com/chat/websocket/config/WebSocketConfig.java
+++ src/main/java/com/chat/websocket/config/WebSocketConfig.java
@@ -35,7 +35,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     @Override
